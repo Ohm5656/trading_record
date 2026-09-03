9bd67aa0055aa090e899307af320e4b59e1d1106
Ohm5656 <natdanailunaha@gmail.com>
2026-09-03 20:51:25 +0700

    feat: add installable offline PWA support

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'edge-journal-v2'
+const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/app-icon.svg']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  const requestUrl = new URL(event.request.url)
+  if (requestUrl.origin !== self.location.origin) return
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fromNetwork = fetch(event.request)
+        .then((response) => {
+          if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()))
+          return response
+        })
+        .catch(() => cached || (event.request.mode === 'navigate' ? caches.match('/index.html') : undefined))
+
+      return cached || fromNetwork
+    }),
+  )
+})
